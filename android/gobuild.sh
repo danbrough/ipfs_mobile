1be1714bf8ce02cef60c72da95a3a9643a55a51b
Dan Brough <dan@danbrough.org>
2021-09-23 07:06:17 +1200

    update

--- android/gobuild.sh
+++ android/gobuild.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 echo running $0 at `date` at `pwd`
+cd ..
 
-mkdir tmp && cd tmp
-wget https://golang.org/dl/go1.16.8.linux-amd64.tar.gz > /dev/null 2>&1 || exit 1
-tar xvpf go1.16.8.linux-amd64.tar.gz > /dev/null 2>&1
+#wget https://golang.org/dl/go1.16.8.linux-amd64.tar.gz > /dev/null 2>&1 || exit 1
+#tar xvpf go1.16.8.linux-amd64.tar.gz > /dev/null 2>&1
 
+DIR=`pwd`
+export PATH=$DIR/go/bin:$PATH
+export GOPATH=$DIR/gopath
 
-go/bin/go version
+cd $GOPATH
 
+go version
+bash --norc --noprofile -i
 
 
 
