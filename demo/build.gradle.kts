03de145c6ddc5f1694f3ba778761e8ba26dbbf0c
Dan Brough <dan@danbrough.org>
2021-11-10 08:08:15 +1300

    update

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 
-System.getProperties().keys.forEach {
+/*System.getProperties().keys.forEach {
   println("$it -> ${System.getProperty(it?.toString())}")
-}
+}*/
 
 val arch = System.getProperty("os.arch")!!
 val osName = System.getProperty("os.name")!!.let {
@@ -28,6 +28,7 @@ dependencies {
   val libName = "${osName.toLowerCase()}${arch.capitalizeAsciiOnly()}"
   println("libName: $libName")
   implementation("com.github.danbrough.ipfs_mobile:$libName:_")
+  implementation("com.github.danbrough.ipfs_mobile:core:_")
   implementation("com.github.danbrough.ipfs_mobile:jvm:_")
 }
 
