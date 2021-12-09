b419e8bd46578a6a42c00b26eba5db2f5aa84a73
Dan Brough <dan@danbrough.org>
2021-12-10 11:50:06 +1300

    update

--- docker/home/.bashrc
+++ docker/home/.bashrc
@@ -111,3 +111,5 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
+
+export PS1='\[\033[01;33m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
--- docker/setup.sh
+++ docker/setup.sh
@@ -2,5 +2,24 @@
 
 useradd    -M -s /bin/bash kipfs
 
-cp -av /home/kipfs/docker/home/ /home/kipfs/
+cp -av /home/kipfs/docker/home/  -t /home/kipfs/
 chown kipfs:kipfs -R /home/kipfs
+
+[ -d /usr/local/go ] && rm -rf /usr/local/go
+
+ARCH=`uname -m`
+GOVERSION=1.17.5
+
+if [ "$ARCH" = "X86_64" ]; then
+  DOWNLOAD=go$GOVERSION.linux-amd64.tar.gz
+elif [ "$ARCH" = "aarch64" ]; then
+  DOWNLOAD=go$GOVERSION.linux-arm64.tar.gz
+elif [ "$ARCH" = "arm" ]; then
+  DOWNLOAD=go$GOVERSION.linux-armv6l.tar.gz
+fi
+
+
+cd /tmp
+wget https://go.dev/dl/$DOWNLOAD
+
+tar xvpf $DOWNLOAD -C /usr/local
