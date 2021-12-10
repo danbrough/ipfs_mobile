6b1e21a747875a4fc0f0d6dc30a099cf484781f1
Dan Brough <dan@danbrough.org>
2021-12-10 14:13:54 +1300

    update

--- docker/home/build.sh
+++ docker/home/build.sh
@@ -2,27 +2,20 @@
 
 cd ~/ipfs_mobile/go
 
-# for Raspberry-pi 32bit
-if [ "$ARCH" == "arm" ]; then
-  export GOARM=5
-fi
+export PACKAGES="kipfs/core kipfs/cids kipfs/pubsub"
 
 go mod download || exit 1
 go get -d  github.com/danbrough/mobile
-go install  github.com/danbrough/mobile/cmd/gomobile
-go install  github.com/danbrough/mobile/cmd/gobind
-echo running "gomobile init" using `which gomobile`
-gomobile init || exit 1
+#go install  github.com/danbrough/mobile/cmd/gomobile
+#go install  github.com/danbrough/mobile/cmd/gobind
+#echo running "gomobile init" using `which gomobile`
+#gomobile init || exit 1
 
 echo building kipfs
 
-BUILDDIR=/tmp/gobuild
-
-if [ -d $BUILDDIR ]; then
-    rm -rf $BUILDIR
-fi
+BUILDDIR=/home/kipfs/build
 
 go run github.com/danbrough/mobile/cmd/gomobile \
     bind -ldflags "-w" \
     -work -v  \
-    -target=linux/$ARCH -javapkg go.kipfs --tags=openssl -o $BUILDDIR
+    -target=linux/$ARCH -javapkg go.kipfs --tags=openssl -o $BUILDDIR $PACKAGES
--- docker/home/env.sh
+++ docker/home/env.sh
@@ -6,3 +6,8 @@ export GOPATH=$GOPATH:/home/kipfs/ipfs_mobile
 export ANDROID_NDK_ROOT=/opt/ndk
 
 
+# for Raspberry-pi 32bit
+if [ "$ARCH" == "arm" ]; then
+  export GOARM=5
+fi
+
