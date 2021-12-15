e84befa3cdd0a5983df31246509392902564b7d2
Dan Brough <dan@danbrough.org>
2021-12-15 14:44:35 +1300

    0.11.0_06

--- docker/build_linux.sh
+++ docker/build_linux.sh
@@ -22,9 +22,9 @@ export CGO_CFLAGS="-fPIC -static -I$OPENSSL_LIBS/include"
 echo compiling with  -L$OPENSSL_LIBS/lib
 
 export CGO_LDFLAGS="-static -fPIC \
- -L$OPENSSL_LIBS/lib -lcrypto -lssl -ldl "
+ -L$OPENSSL_LIBS/lib  -L/usr/lib/x86_64-linux-gnu -lcrypto -lssl -ldl -lpthread -lc "
 
-gomobile bind -ldflags "-w" -x -v \
+gomobile bind -ldflags "-w" -v \
     -target=linux/$ARCH -javapkg go.kipfs --tags=openssl \
     -o $BUILDDIR/libs/linux $PACKAGES || exit 1
 
