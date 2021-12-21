b81f99368c04734d30f82e4da2b9ff232954417e
Dan Brough <dan@danbrough.org>
2021-12-21 14:47:31 +1300

    update

--- docker/ipfsmobile/run.sh
+++ docker/ipfsmobile/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-amd64=docker.io/danbrough/ipfsmobile:latest@sha256:c4464c75b8131fea2e1a83ad3270cb7086330097c42836913154808b8eaee04f
-arm64=docker.io/danbrough/ipfsmobile:latest@sha256:67080bf5d37024e7b79ca59d5b89460d93646381aca0dd197cdaf5d348246ff9
+amd64=docker.io/danbrough/ipfsmobile:latest@sha256:070f13442093d697026b0bfe36b27fb25e1fb96551e89d44e97dffd29befeb7c
+arm64=docker.io/danbrough/ipfsmobile:latest@sha256:75cfbc8ae2299c8a13a721af85025db6467ca2ddf0820e547b90c14f2ce9a726
 
-PLATFORM=arm64
+PLATFORM=amd64
 NAME=ipfsmobile
 #Place to store cache files
 CACHEDIR=/mnt/files2/cache
